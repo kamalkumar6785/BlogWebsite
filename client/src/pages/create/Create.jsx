d7dd816d4899ec00f4116b580bfdcf399fd61f2f
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-08-01 18:51:27 +0530

    quill added

--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -4,51 +4,76 @@ import "react-quill/dist/quill.snow.css";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
-
+import { storage } from "../../Firebase"; 
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import '../../styles/Create.css'
 
 const Write = () => {
   const state = useLocation().state;
-  const [value, setValue] = useState(state?.title || "");
-  const [title, setTitle] = useState(state?.desc || "");
+  const [value, setValue] = useState(state?.desc || "");
+  const [title, setTitle] = useState(state?.title || "");
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState(state?.cat || "");
-
-  const navigate = useNavigate()
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
 
   const upload = async () => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
-      const res = await axios.post("/upload", formData);
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
+    if (!file) return "";
+
+    return new Promise((resolve, reject) => {
+      const storageRef = ref(storage, `images/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+        
+        },
+        (error) => {
+          console.error("Error uploading file:", error);
+          setError("Error uploading file. Please try again.");
+          reject(error);
+        },
+        async () => {
+          try {
+            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+            console.log(downloadURL);
+            resolve(downloadURL);
+          } catch (error) {
+            console.error("Error getting download URL:", error);
+            setError("Error retrieving file URL. Please try again.");
+            reject(error);
+          }
+        }
+      );
+
+
+    });
   };
 
   const handleClick = async (e) => {
     e.preventDefault();
-    const imgUrl = await upload();
 
     try {
-      state
-        ? await axios.put(`/posts/${state.id}`, {
-            title,
-            desc: value,
-            cat,
-            img: file ? imgUrl : "",
-          })
-        : await axios.post(`/posts/`, {
-            title,
-            desc: value,
-            cat,
-            img: file ? imgUrl : "",
-            date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-          });
-          navigate("/")
+      const imgUrl = await upload();
+      const postData = {
+        title,
+        desc: value,
+        cat,
+        img: file ? imgUrl : "",
+        date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      };
+
+      if (state) {
+        await axios.put(`/posts/${state.id}`, postData);
+      } else {
+        await axios.post(`/posts/`, postData);
+      }
+
+      navigate("/");
     } catch (err) {
-      console.log(err);
+      console.error(err);
+      setError("Error publishing blog. Please try again.");
     }
   };
 
@@ -58,6 +83,7 @@ const Write = () => {
         <input
           type="text"
           placeholder="Title"
+          value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
         <div className="editorContainer">
@@ -71,41 +97,10 @@ const Write = () => {
       </div>
       <div className="menu">
         <div className="item">
-          <h1>Publish</h1>
-          <span>
-            <b>Status: </b> Draft
-          </span>
-          <span>
-            <b>Visibility: </b> Public
-          </span>
-          <input
-            style={{ display: "none" }}
-            type="file"
-            id="file"
-            name=""
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <label className="file" htmlFor="file">
-            Upload Image
-          </label>
-          <div className="buttons">
-            <button>Save as a draft</button>
-            <button onClick={handleClick}>Publish</button>
+          <div style={{ textAlign: 'center' }}>
+            <h1>Select Category</h1>
           </div>
-        </div>
-        <div className="item">
-          <h1>Category</h1>
-          <div className="cat">
-            <input
-              type="radio"
-              checked={cat === "art"}
-              name="cat"
-              value="art"
-              id="art"
-              onChange={(e) => setCat(e.target.value)}
-            />
-            <label htmlFor="art">Art</label>
-          </div>
+
           <div className="cat">
             <input
               type="radio"
@@ -115,53 +110,103 @@ const Write = () => {
               id="science"
               onChange={(e) => setCat(e.target.value)}
             />
-            <label htmlFor="science">Science</label>
+            <label htmlFor="science">Science 🚀</label>
           </div>
+
           <div className="cat">
             <input
               type="radio"
-              checked={cat === "technology"}
+              checked={cat === "art"}
               name="cat"
-              value="technology"
-              id="technology"
+              value="art"
+              id="art"
               onChange={(e) => setCat(e.target.value)}
             />
-            <label htmlFor="technology">Technology</label>
+            <label htmlFor="art">Art 🎨</label>
           </div>
+
           <div className="cat">
             <input
               type="radio"
-              checked={cat === "cinema"}
+              checked={cat === "entertainment"}
               name="cat"
-              value="cinema"
-              id="cinema"
+              value="entertainment"
+              id="entertainment"
               onChange={(e) => setCat(e.target.value)}
             />
-            <label htmlFor="cinema">Cinema</label>
+            <label htmlFor="entertainment">Entertainment 🎬</label>
           </div>
+
           <div className="cat">
             <input
               type="radio"
-              checked={cat === "design"}
+              checked={cat === "finance"}
               name="cat"
-              value="design"
-              id="design"
+              value="finance"
+              id="finance"
               onChange={(e) => setCat(e.target.value)}
             />
-            <label htmlFor="design">Design</label>
+            <label htmlFor="finance">Finance 💲</label>
           </div>
+
           <div className="cat">
             <input
               type="radio"
-              checked={cat === "food"}
+              checked={cat === "health"}
               name="cat"
-              value="food"
-              id="food"
+              value="health"
+              id="health"
               onChange={(e) => setCat(e.target.value)}
             />
-            <label htmlFor="food">Food</label>
+            <label htmlFor="health">Health 🩺</label>
+          </div>
+
+          <div className="cat">
+        <input
+            type="radio"
+            checked={cat === "gaming"}
+            name="cat"
+            value="gaming"
+            id="gaming"
+            onChange={(e) => setCat(e.target.value)}
+        />
+        <label htmlFor="gaming">Gaming 🎮</label>
+        </div>
+
+
+        <div className="cat">
+        <input
+            type="radio"
+            checked={cat === "news"}
+            name="cat"
+            value="news"
+            id="news"
+            onChange={(e) => setCat(e.target.value)}
+        />
+        <label htmlFor="news">NEWS 📰</label>
+        </div>
+
+
+        {/* Adding a margin-bottom to the last category */}
+        <div style={{ marginBottom: '20px' }}></div> 
+
+        <input
+        style={{ display: "none" }}
+        type="file"
+        id="file"
+        name="file"
+        onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label className="file" htmlFor="file" style={{ marginTop: '20px' }}>
+        Upload Image
+        </label>
+
+
+          <div className="buttons">
+            <button  onClick={handleClick}>Publish Blog</button>
           </div>
         </div>
+        {err && <p className="error">{err}</p>}
       </div>
     </div>
   );
